4ca70aa2eaa47d1ba827ae7a39b21dd14aeece28
Justin <39439980+Justin6842@users.noreply.github.com>
2024-12-10 14:28:22 -0500

    Add files via upload

---
+++ train.sql
@@ -0,0 +1,93 @@
+CREATE DATABASE IF NOT EXISTS `TrainDatabase`;
+USE `TrainDatabase`;
+
+-- TrainSchedule table
+CREATE TABLE IF NOT EXISTS TrainSchedule (
+    transitLineName VARCHAR(50),
+    Train VARCHAR(50),
+    Origin VARCHAR(50),
+    Destination VARCHAR(50),
+    departDate DATE,
+    arrivalDate DATE,
+    travelTime VARCHAR(50),
+    stopsAmount INT,
+    fare FLOAT,
+    PRIMARY KEY (transitLineName, Train)
+);
+
+-- hasTrainStation table
+CREATE TABLE IF NOT EXISTS hasTrainStation (
+    transitLineName VARCHAR(50),
+    stationID INT,
+    name VARCHAR(50),
+    cityName VARCHAR(50),
+    state VARCHAR(50),
+    PRIMARY KEY (stationID, transitLineName),
+    FOREIGN KEY (transitLineName) REFERENCES TrainSchedule(transitLineName)
+);
+
+-- TrainStops table
+CREATE TABLE IF NOT EXISTS TrainStops (
+    transitLineName VARCHAR(50),
+    Train VARCHAR(50),
+    stopOrder INT,
+    stationID INT,
+    PRIMARY KEY (transitLineName, Train, stopOrder),
+    FOREIGN KEY (transitLineName, Train) REFERENCES TrainSchedule(transitLineName, Train),
+    FOREIGN KEY (stationID) REFERENCES hasTrainStation(stationID)
+);
+
+-- Customer table
+CREATE TABLE IF NOT EXISTS Customer (
+    emailAddress VARCHAR(50),
+    lastName VARCHAR(50),
+    firstName VARCHAR(50),
+    username VARCHAR(50),
+    password VARCHAR(50),
+    PRIMARY KEY (emailAddress)
+);
+
+-- Reservation table
+CREATE TABLE IF NOT EXISTS Reservation (
+    Number INT,
+    emailAddress VARCHAR(50),
+    transitLineName VARCHAR(50),
+    Train VARCHAR(50),
+    Origin VARCHAR(50),
+    Destination VARCHAR(50),
+    departDate DATE,
+    arrivalDate DATE,
+    fare FLOAT,
+    Passenger VARCHAR(50),
+    totalFare FLOAT,
+    PRIMARY KEY (Number),
+    FOREIGN KEY (emailAddress) REFERENCES Customer(emailAddress),
+    FOREIGN KEY (transitLineName, Train)
+        REFERENCES TrainSchedule(transitLineName, Train)
+);
+
+-- Employees table
+CREATE TABLE IF NOT EXISTS Employee (
+    SSN CHAR(11),
+    lastName VARCHAR(50),
+    firstName VARCHAR(50),
+    username VARCHAR(50),
+    password VARCHAR(50),
+    role ENUM('Admin', 'Employee'),
+    PRIMARY KEY (SSN)
+);
+
+-- CustomerIssues table
+CREATE TABLE IF NOT EXISTS CustomerIssues (
+    emailAddress VARCHAR(50),
+    issueDescription VARCHAR(1000),
+    response VARCHAR(1000),
+    salesRepSSN CHAR(11),
+    PRIMARY KEY (emailAddress, salesRepSSN),
+    FOREIGN KEY (emailAddress) REFERENCES Customer(emailAddress),
+    FOREIGN KEY (salesRepSSN) REFERENCES Employees(SSN)
+);
+
+INSERT INTO `Customer` VALUES 
+('hi@gmail.com', 'hey', 'hello', 'cust1', 'pass123');
+
